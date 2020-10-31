c79c8a821e69cc5082691ac64f091f5129c9ed5d
Gabriel Cunha <gabrielrcunha15@gmail.com>
2020-10-31 19:32:12 -0300

    + model de estatistica

---
+++ src/model/Estatistica.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public abstract class Estatistica {
+	private String nome;
+	private List<Medicao> observacoes;
+	
+	public Estatistica(String nome, List<Medicao> observacoes) {
+		this.nome = nome;
+		this.observacoes = observacoes;
+	}
+	
+	public boolean inclui(Medicao observacao) {
+		return observacoes.contains(observacao);
+	}
+	
+	public LocalDate dataInicio() {
+		Collections.sort(observacoes);
+		return observacoes.get(0).getMomento().toLocalDate();
+	}
+	
+	public LocalDate dataFim() {
+		Collections.sort(observacoes);
+		return observacoes.get(observacoes.size()-1).getMomento().toLocalDate();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Medicao> getObservacoes() {
+		return new ArrayList<>(observacoes);
+	}
+	
+	//TODO: adicionar função valor()
+}
