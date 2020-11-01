647fa3207abbff8aead4a3e901f82a10cc25f9fb
Gabriel Cunha <gabrielrcunha15@gmail.com>
2020-11-01 18:53:32 -0300

    + classe CrescimentoPeriodo

---
+++ src/controller/estatisticas/CrescimentoPeriodo.java
@@ -0,0 +1,28 @@
+package controller.estatisticas;
+
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Estatistica;
+import model.Medicao;
+
+public class CrescimentoPeriodo extends Estatistica {
+
+	public CrescimentoPeriodo(String nome, List<Medicao> observacoes) {
+		super(nome, observacoes);
+	}
+
+	@Override
+	public float valor() {
+		ArrayList<Medicao> medicoes = new ArrayList<Medicao>(getObservacoes());
+		Medicao primeira = medicoes.get(0);
+		Medicao ultima = medicoes.get(medicoes.size() - 1);
+		long dias = primeira.getMomento().until(ultima.getMomento(), ChronoUnit.DAYS);
+		
+		if (dias == 0) return 0;
+		
+		return (ultima.getCasos() - primeira.getCasos())/dias;
+	}
+
+}
