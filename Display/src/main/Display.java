31e8a1efe860e23281e7d9749005f3d19ad894c0
Mari <mari-191938@hotmail.com>
2020-11-05 23:48:26 -0300

    Display em formato de tabela

--- Display/src/main/Display.java
+++ Display/src/main/Display.java
@@ -1,47 +1,80 @@
 package main;
-
-import java.awt.Font;
-import java.awt.GraphicsConfiguration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
-
-public class Display{
-	
-	public void frame() {
-		JFrame frame;
-		JLabel label;
-		JTextArea text;
-		StringBuilder ranking = new StringBuilder();
-		Map <String, Double> demo = new HashMap<>();
-		demo.put("Arroz", 1.0);
-		demo.put("Feijao", 2.0);
-		demo.put("Sardinha", 3.0);
+public class Display {
+	JFrame frame = null;
+	JTable table = null;
+	JScrollPane scroll = null;
+	FlowLayout layout = new FlowLayout();
+	public  void Display(String Titulo, String[][] dados, String[] colunas) {
 		
-		frame = new JFrame("Display Ranking comida");
-		text = new JTextArea();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().add(text);
-		text.setLineWrap(true);
-		text.setWrapStyleWord(true);
-		text.setFont(new Font("Arial",Font.BOLD,20));
-		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		frame.pack();
+		frame = new JFrame();
+		table = new JTable(dados,colunas);
+		frame.setLayout(layout);
+		frame.setSize(800, 600);
+		frame.setLocationRelativeTo(null);
+		frame.setTitle(Titulo);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		table.setPreferredScrollableViewportSize(new Dimension(700,400));
+		table.setFillsViewportHeight(true);
+		table.setEnabled(false);
+		scroll = new JScrollPane(table);
+		frame.add(scroll);
 		frame.setVisible(true);
 		
-		for(Entry<String, Double> key : demo.entrySet()) {
-			String temp = key.getKey()+": "+key.getValue().toString()+"\n";
-			ranking.append(temp);
-			
-		}
-		text.setText(ranking.toString());
-		System.out.println(ranking);
+		backButton();
+		exportButton();
+		
+	}
+	
+	public void backButton() {
+		JButton back = new JButton("Voltar");
+		frame.add(back);
+		layout.setAlignment(FlowLayout.LEFT);
+		
+		back.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		
+	
+	}
+		);
+	}
+	
+	public void exportButton() {
+		JButton export = new JButton("Exportar");
+		frame.add(export);
+		layout.setAlignment(FlowLayout.RIGHT);
+		layout.setAlignment(FlowLayout.CENTER);
+		export.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		
+	
+	}
+		);
 	}
 	
 	public static void main(String[] args) {
+		String colunas[] = {"Países","Numero de mortos"};
+		String dados[][] = {{"Estados unidos","22"},
+				{"Brasil","32"},
+				{"Inglaterra","25"}
+		};
+		
 		Display display = new Display();
-		display.frame();
+		display.Display("Ranking", dados, colunas);
 	}
 }
