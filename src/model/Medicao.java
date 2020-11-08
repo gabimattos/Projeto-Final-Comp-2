457b73ff9595090cd5a4ec8b85452d83ca12b77c
Gabriel Cunha <gabrielrcunha15@gmail.com>
2020-11-07 23:00:59 -0300

    + documentação de medição

--- src/model/Medicao.java
+++ src/model/Medicao.java
@@ -3,6 +3,14 @@ package model;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+/**
+ * Esta classe representa uma leitura dos dados de COVID-19, para um determinado
+ * pais, em um determinado momento. Uma medicao pode representar total de mortos,
+ * casos confirmados ou recuperados.
+ *
+ * @author Gabriel Rodrigues Cunha - 119.143.696
+ *
+ */
 public class Medicao implements Comparable<Medicao>, Serializable{
 	private static final long serialVersionUID = 1000L;
 	private Pais pais;
@@ -10,6 +18,14 @@ public class Medicao implements Comparable<Medicao>, Serializable{
 	private int casos;
 	private StatusCaso status;
 	
+	/**
+	 * Cria uma instancia de Medicao com os dados fornecidos.
+	 * 
+	 * @param pais	Pais da medicao.
+	 * @param momento	Momento em que a medicao ocorreu.
+	 * @param casos	Numero de casos registrado pela medicao.
+	 * @param status	Tipo da medicao (mortos, casos confirmados ou recuperados).
+	 */
 	public Medicao(Pais pais, LocalDateTime momento, int casos, StatusCaso status) {
 		this.pais = pais;
 		this.momento = momento;
@@ -17,34 +33,74 @@ public class Medicao implements Comparable<Medicao>, Serializable{
 		this.status = status;
 	}
 
+	/**
+	 * Funcao getter do pais da medicao.
+	 * 
+	 * @return	Pais da medicao.
+	 */
 	public Pais getPais() {
 		return pais;
 	}
 
+	/**
+	 * Funcao setter do pais da Medicao.
+	 * 
+	 * @param pais	Novo Pais da medicao.
+	 */
 	public void setPais(Pais pais) {
 		this.pais = pais;
 	}
 
+	/**
+	 * Funcao getter do momento da medicao.
+	 * 
+	 * @return	Momento da medicao.
+	 */
 	public LocalDateTime getMomento() {
 		return momento;
 	}
 
+	/**
+	 * Funcao setter do momento da Medicao.
+	 * 
+	 * @param momento	Novo momento da medicao.
+	 */
 	public void setMomento(LocalDateTime momento) {
 		this.momento = momento;
 	}
 
+	/**
+	 * Funcao getter do No. de casos da medicao.
+	 * 
+	 * @return	No. de casos da medicao.
+	 */
 	public int getCasos() {
 		return casos;
 	}
 
+	/**
+	 * Funcao setter do No. de casos da Medicao.
+	 * 
+	 * @param casos	Novo No. de casos da medicao.
+	 */
 	public void setCasos(int casos) {
 		this.casos = casos;
 	}
 
+	/**
+	 * Funcao getter do tipo da medicao.
+	 * 
+	 * @return	tipo da medicao (mortos, casos confirmados ou recuperados).
+	 */
 	public StatusCaso getStatus() {
 		return status;
 	}
 
+	/**
+	 * Funcao setter do tipo da Medicao.
+	 * 
+	 * @param status	Novo tipo da medicao mortos, casos confirmados ou recuperados).
+	 */
 	public void setStatus(StatusCaso status) {
 		this.status = status;
 	}
