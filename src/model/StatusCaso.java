5719f85c09ecf5ba1f813b3fedf88b942b5091e7
Gabriel Cunha <gabrielrcunha15@gmail.com>
2020-11-07 23:25:48 -0300

    + documentação de StatusCaso

--- src/model/StatusCaso.java
+++ src/model/StatusCaso.java
@@ -1,5 +1,11 @@
 package model;
 
+/**
+ * Este enum representa os diferentes tipos de medicoes de casos da COVID-19.
+ *
+ * @author Gabriel Rodrigues Cunha - 119.143.696
+ *
+ */
 public enum StatusCaso {
 	CONFIRMADOS("confirmados"),
 	RECUPERADOS("recuperados"),
@@ -11,6 +17,11 @@ public enum StatusCaso {
 		this.nome = nome;
 	}
 
+	/**
+	 * Rertorna o nome do StatusCaso.
+	 * 
+	 * @return nome do StatusCaso.
+	 */
 	public String getNome() {
 		return nome;
 	}
